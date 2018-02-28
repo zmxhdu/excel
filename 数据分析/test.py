cf2bb89c596122e9ef3510d95e2c9097dbce2aa1
XQUANT-PC015A\Administrator <zmxhdu@126.com>
2018-02-28 17:31:11 +0800

    20180228

--- 数据分析/test.py
+++ 数据分析/test.py
@@ -1,3 +1,43 @@
 # coding = utf-8
 
 import numpy as np
+import pandas as pd
+
+
+df = pd.read_csv("DataAnalyst.csv", encoding='gb2312')
+
+# 根据 positionId 去重
+df_duplicates = df.drop_duplicates(subset='positionId', keep='first')
+
+
+# 获取薪资上下限
+def cut_word(word, method):
+    position = word.find('-')
+    length = len(word)
+    if position != -1:
+        bottomSalary = word[:position-1]
+        topSalary = word[position + 1:length - 1]
+    else:
+        bottomSalary = word[:word.upper().find('K')]
+        topSalary = bottomSalary
+    if method == 'bottom':
+        return bottomSalary
+    else:
+        return topSalary
+
+
+df_duplicates = df_duplicates.copy() # copy下，防止警告SettingWithCopyWarning
+df_duplicates['bottomSalary'] = df_duplicates.salary.apply(cut_word, method='bottom')
+df_duplicates['topSalary'] = df_duplicates.salary.apply(cut_word, method='top')
+
+# 转换成 int 格式
+df_duplicates.bottomSalary = df_duplicates.bottomSalary.astype('int')
+df_duplicates.topSalary = df_duplicates.topSalary.astype('int')
+
+# 计算平均工资
+df_duplicates['avgSalary'] = df_duplicates.apply(lambda x:(x.bottomSalary+x.topSalary)/2, axis=1)
+
+df_clean = df_duplicates[['city', 'companyShortName', 'companySize', 'education', 'positionName', 'positionLables',
+                          'workYear', 'bottomSalary', 'topSalary', 'avgSalary']]
+
+print(df_clean.head())
--- 新建文件夹/color.py
+++ 新建文件夹/color.py
@@ -5,7 +5,7 @@ from openpyxl.styles import Color, Fill
 wb = openpyxl.load_workbook('工作簿1.xlsx')
 sheet = wb.get_active_sheet()
 
-# for i in range(2, sheet.max_row+1):
-#     for j in range(5, sheet.max_column+1):
-#         cell = sheet.cell(row=i,column=j)
-#         print(cell.fill)
+for i in range(1, sheet.max_row):
+    for j in range(1, sheet.max_column):
+        cell = sheet.cell(row=i,column=j)
+        print(cell.fill)
