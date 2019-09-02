77946798710d9df56d194272c2ebde2c9184a203
XQUANT-PC015A\Administrator <zmxhdu@126.com>
2019-09-02 18:30:28 +0800

    123

--- selenium-test/xQA-interfaces/xQA-interfaces.py
+++ selenium-test/xQA-interfaces/xQA-interfaces.py
@@ -1,8 +1,10 @@
-# coding=utf-8
+# coding = utf-8
 import json
+import pandas as pd
+import numpy as np
 import requests
 import datetime
-from market_interfaces.market_interfaces_tidx import CalcTidxForReal,CalcTidxInvlForReal,CalcTidx
+from market_interfaces.market_interfaces_tidx import CalcTidxForReal, CalcTidxInvlForReal, CalcTidx
 from market_interfaces.market_interfaces_bond import CalcBond
 
 
@@ -34,16 +36,37 @@ if __name__ == '__main__':
     # interfaces_data, res = interface_tidx.result()
     # res = interface_tidx.result().text
 
-    instrumentList = [{'iCode': '151096', 'aType': 'SPT_BD', 'mType': 'XSHG'}]
+    instrumentList = [{'iCode': '151096', 'aType': 'SPT_BD', 'mType': 'XSHG'},
+                      {'iCode': '151098', 'aType': 'SPT_BD', 'mType': 'XSHG'}]
     interface_bond = CalcBond(postUrl, header_data, instrumentList, valueDate)
     interfaces_data, res = interface_bond.result()
     result = json.loads(res.text)
-    # print(interfaces_data)
+    # print(result['result'])
+
+    result_df = pd.DataFrame()
 
     if result['code'] == '-1':
         print(result['message'])
     else:
-        for result_detail in result['result']:
-            for key, value in result_detail.items():
-                print(key, value)
-            # print(result_detail, len(result_detail))
+        for result_detail, instrument in zip(result['result'], instrumentList):
+            # for instrument_key, instrument_value in instrument.items():
+            #     if type(instrument_value) is list:
+            #         for i in range(0, len(instrument_value)):
+            #             index = instrument_key + '_' + str(i)
+            #             print(index, instrument_value[i])
+            #     else:
+            #         print(instrument_key, instrument_value)
+            # for result_key, result_value in result_detail.items():
+            #     if type(result_value) is list:
+            #         for i in range(0, len(result_value)):
+            #             index = result_key + '_' + str(i)
+            #             print(index, result_value[i])
+            #     else:
+            #         print(result_key, result_value)
+            instrument_df_detail = pd.DataFrame(instrument, index=[0])
+            result_df_detail = pd.DataFrame(result_detail)
+            # print(result_df_detail)
+            # result_df_detail = pd.merge(instrument_df_detail, result_df_detail)
+
+            # result_df.merge(result_df,result_df_detail)
+            result_df_detail.to_excel('test.xlsx')
