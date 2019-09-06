93abf17d5f1524c4ca2746dcad0cca997b72b1fc
XQUANT-PC015A\Administrator <zmxhdu@126.com>
2019-09-06 17:24:52 +0800

    123

--- selenium-test/xQA-interfaces/interfaces/interfaces_conditions/market_conditions.py
+++ selenium-test/xQA-interfaces/interfaces/interfaces_conditions/market_conditions.py
@@ -1,33 +1,37 @@
 # coding = utf-8
 import connect_to_oracle
 import configparser
+import json
 
 
-TIDX_SQL = 'select I_CODE,A_TYPE,M_TYPE from TIDX order by I_CODE'
-TFND_SQL = 'select I_CODE,A_TYPE,M_TYPE from TFND order by I_CODE'
-STOCK_SQL = 'select I_CODE,A_TYPE,M_TYPE from TSTK order by I_CODE'
-TBND_SQL = 'select I_CODE,A_TYPE,M_TYPE from TBND order by I_CODE'
-CASHLB_SQL = 'select I_CODE,A_TYPE,M_TYPE from TTRD_CASHLB order by I_CODE'
-PAYMENT_SQL = 'select I_CODE,A_TYPE,M_TYPE from TBND order by I_CODE ' \
-              'union all ' \
-              'select I_CODE,A_TYPE,M_TYPE from TTRD_CASHLB order by I_CODE'
-BONDFUTUER_SQL = 'select I_CODE,A_TYPE,M_TYPE from TBNDFUTURE order by I_CODE'
-STOCKIDXFUTUER_SQL = 'select I_CODE,A_TYPE,M_TYPE from TSTK_IDX_FUTURE order by I_CODE'
-CVTBOND_SQL = 'select I_CODE,A_TYPE,M_TYPE from TBND where A_TYPE=:A_TYPE order by I_CODE'
-CVTBOND_PARAMS = {'A_TYPE':'SPT_CB'}
-STOCKOPTION_SQL = 'select I_CODE,A_TYPE,M_TYPE from TEQOPTION order by I_CODE'
-TIRSWAP_SQL = 'select I_CODE,A_TYPE,M_TYPE from TEQOPTION order by I_CODE'
+class Market_Conditions(object):
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read('config.config')
+        database = 'database_66'
+        SQL = 'SQL'
 
-config = configparser.ConfigParser()
-config.read('config.config')
-database = 'database_66'
+        db = connect_to_oracle.DataBase(
+            host=config.get(database, 'host'),
+            port=config.get(database, 'port'),
+            dbname=config.get(database, 'dbname'),
+            username=config.get(database, 'username'),
+            password=config.get(database, 'password')
+        )
 
-db = connect_to_oracle.DataBase(
-        host=config.get(database, 'host'),
-        port=config.get(database, 'port'),
-        dbname=config.get(database, 'dbname'),
-        username=config.get(database, 'username'),
-        password=config.get(database, 'password')
-    )
-res = db.sql(TIDX_SQL)
-print(res)
+        self.db =db
+        self.config = config
+        self.SQL = SQL
+
+    def get_sql(self, type):
+        sql = self.config.get(self.SQL, type.upper()+'_SQL').replace('\n',' ')
+        params = json.loads(self.config.get(self.SQL, type.upper()+'_PARAMS'))
+        fields = self.config.get(self.SQL, type.upper()+'_FIELDS').replace(' ','').split(',')
+        sql_result = self.db.sql(sql, params)
+
+        resultList = []
+        for result in sql_result:
+            param = dict(zip(fields, list(result)))
+            resultList.append(param)
+
+        return resultList
--- selenium-test/xQA-interfaces/xQA-interfaces.py
+++ selenium-test/xQA-interfaces/xQA-interfaces.py
@@ -1,54 +1,43 @@
 # coding = utf-8
 import json
-from market_interfaces.market_interfaces_tidx import CalcTidxForReal, CalcTidxInvlForReal, CalcTidx
-from market_interfaces.market_interfaces_bond import CalcBond
-from market_interfaces_batch.market_interfaces_bond_batch import CalcBond_Batch
+import market_interfaces
+import acct_interfaces
+from market_interfaces_batch import market_interfaces_bond_batch, market_interfaces_fund_batch, \
+    market_interfaces_stock_batch, market_interfaces_tidx_batch
+from market_interfaces import market_interfaces_tidx, market_interfaces_bond, market_interfaces_bondfutuer, \
+    market_interfaces_cashlb, market_interfaces_cvtbond, market_interfaces_fund, market_interfaces_payment, \
+    market_interfaces_stock, market_interfaces_stockidxfutuer, market_interfaces_stockoption, market_interfaces_tirswap
+from acct_interfaces import acct_interfaces_brinson, acct_interfaces_hld, acct_interfaces_liquid, acct_interfaces_market, acct_interfaces_return
 import interfaces_save
-import connect_to_oracle
-import acct_conditions, market_conditions,market_batch_conditions
-
-
-# 批量接口
-postUrl = 'http://191.168.6.6:8180/xalms/engine/calc.action'
-postUrl_batch = 'http://191.168.6.6:8180/xalms/engine/calcBatch.action'
-
-payloadHeader = {
-    'Host': '191.168.6.6:8180',
-    'Content-Type': 'application/json',
-}
-# 下载超时
-timeOut = 250
-
-host = '191.168.6.6'
-port = '1521'
-dbname = 'xalms'
-username = 'xalms_trd'
-password = 'xpar'
-
-db = connect_to_oracle.DataBase(host, port, dbname, username, password)
+from interfaces_conditions import acct_conditions, market_conditions
+import configparser
 
 
 if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('config.config')
+    postUrl = config.get('POST', 'postUrl')
+    postUrl_batch = config.get('POST', 'postUrl_batch')
+    payloadHeader = json.loads(config.get('POST', 'payloadHeader'))
+
     header_data = payloadHeader
-    # instrumentList = [{'iCode': '000300', 'aType': 'IDX_S', 'mType': 'XSHG'},
-    #                   {'iCode': '000016', 'aType': 'IDX_S', 'mType': 'XSHG'}]
     begDate = '2019-06-01'
     endDate = '2019-06-30'
     valueDate = '2019-06-30'
     sampleLenth = '0'
 
-    instrumentList = [{'iCode': '151098', 'aType': 'SPT_BD', 'mType': 'XSHG'},
-                      {'iCode': '151096', 'aType': 'SPT_BD', 'mType': 'XSHG'}]
     iCode = '151098'
     aType = 'SPT_BD'
     mType = 'XSHG'
     instrument = {'iCode': '151098',
                   'aType': 'SPT_BD',
                   'mType': 'XSHG'}
-    interface_bond_batch = CalcBond_Batch(postUrl_batch, header_data, instrumentList, valueDate)
+    instrumentList = market_conditions.Market_Conditions().get_sql('TBND_BATCH')
+    interface_bond_batch = market_interfaces_bond_batch.CalcBondForReal_Batch(postUrl_batch, header_data, instrumentList)
+
     # interface_bond = CalcBond(postUrl, header_data, iCode, aType, mType, valueDate)
     interfaces_data, res = interface_bond_batch.result()
     resultList = json.loads(res.text)
     # interfaces_save.result_save(instrument, resultList, excelname='CalcBond', sheetname='CalcBond')
-    interfaces_save.batch_result_save(instrumentList, resultList, excelname='CalcBond', sheetname='CalcBond')
-
+    # interfaces_save.batch_result_save(instrumentList, resultList, excelname='CalcBond', sheetname='CalcBond')
+    interfaces_save.batch_result_save(instrumentList, resultList, excelname='TIDX.xlsx', sheetname='TIDX')
